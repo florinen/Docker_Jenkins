0ec9cd91087ea5203fbf46737ab57d502bfe8b84
florinen <nenfl24@gmail.com>
2019-03-24 20:26:30 +0000

    dockerfile created

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM centos:centos7
+MAINTAINER "nenfl@yahoo.com"
+
+COPY .   /app/
+WORKDIR /app
+
+EXPOSE 4000
+RUN yum install epel-release -y
+RUN yum install python-pip -y
+RUN pip install -r /app/app/requirements.txt
+
+ENTRYPOINT [ "python", "/app/app/app.py" ]
+
+
---
+++ app/Jenkinsfile.groovy
@@ -0,0 +1,39 @@
+node{
+    properties([parameters([string(defaultValue: '127.0.0.1', description: 'give ip to build s site', name: 'ip', trim: false)])])
+    stage("Install git"){
+        sh "ssh ec2-user@${IP}  sudo yum install git python-pip -y"
+    }
+    stage("Clone a repo"){
+        git 'git@github.com:florinen/Flaskex.git'
+    }
+    stage("Run App"){
+        try{
+           sh "ssh ec2-user@${IP}  sudo mkdir /flaskex 2> /dev/null"
+        }
+        catch(exec){
+           sh "echo folder exists" 
+        }
+    }
+    stage("Copy files"){
+        sh "scp -r * ec2-user@${IP}:/home/ec2-user/"
+    }
+    stage("Move files"){
+        try{
+        sh "ssh ec2-user@${IP}  sudo mv /home/ec2-user/* /flaskex"
+        }
+        catch (exec){
+            sh "echo files/folder exists"
+        }
+    }
+
+    stage("Install requirements"){
+        sh "ssh ec2-user@${IP}     sudo pip install -r /flaskex/requirements.txt"
+    }
+    stage("move service to /etc"){
+        sh "ssh ec2-user@${IP}  sudo mv /flaskex/flaskex.service /etc/systemd/system"
+    }
+    stage("Start service"){
+        sh "ssh ec2-user@${IP}  sudo systemctl start flaskex"
+    }
+    
+}
