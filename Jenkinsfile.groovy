9ffdbfdc426ac92f3efaec2f26dbce7bebf0af7d
florinen <florinen>
2019-03-24 16:07:19 -0500

    updated file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,15 +2,17 @@ node{
      properties([parameters([string(defaultValue: '127.0.0.1', description: 'Give ip to build', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'what version would like to deploy', name: 'Ver', trim: true)])])
     stage("Remove Container") {
         try{
-            sh "ssh root@IP docker rm -f Flaskex"
+            sh "ssh root@${IP} docker rm -f Flaskex"
+
+        }
         catch(exec){
             sh "echo container deleted"
 
-        }
+        
         }
     }
     
     stage("Run Container"){
-        sh "ssh root@IP docker run -d --name Flaskex -p 4000:4000 florinen/fleskex"
+        sh "ssh root@${IP} docker run -d --name Flaskex -p 4000:4000 florinen/fleskex"
     }
 }
