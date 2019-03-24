9672364243212afd770ef6e72aabee4a9f812587
florinen <florinen>
2019-03-24 16:04:42 -0500

    updatefd files

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,12 @@
 node{
-    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Specify your ip', name: 'IP', trim: true)])])
+     properties([parameters([string(defaultValue: '127.0.0.1', description: 'Give ip to build', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'what version would like to deploy', name: 'Ver', trim: true)])])
     stage("Remove Container") {
         try{
             sh "ssh root@IP docker rm -f Flaskex"
+        catch(exec){
+            sh "echo container deleted"
+
+        }
         }
     }
     
