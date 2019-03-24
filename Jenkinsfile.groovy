d12a7239b0e4844df47a86e28dac703c9f4f6e2e
florinen <florinen>
2019-03-24 16:13:37 -0500

    added port

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 node{
-     properties([parameters([string(defaultValue: '127.0.0.1', description: 'Give ip to build', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'what version would like to deploy', name: 'Ver', trim: true)])])
+     properties([parameters([string(defaultValue: '157.230.49.76', description: 'Give ip to build', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'what version would like to deploy', name: 'Ver', trim: true)])])
     stage("Remove Container") {
         try{
             sh "ssh root@${IP} docker rm -f Flaskex"
@@ -13,6 +13,6 @@ node{
     }
     
     stage("Run Container"){
-        sh "ssh root@${IP} docker run -d --name Flaskex -p 4400:4000 florinen/flaskex_centos7:${VER}"
+        sh "ssh root@${IP} docker run -d --name Flaskex -p ${PORT}:4000 florinen/flaskex_centos7:${VER}"
     }
 }
